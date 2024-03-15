b0f0cf137447389d06d997d2002c8a8841d17250
sweetestmeditation <brendan.ngwanforbi@gmail.com>
2024-03-14 21:22:27 -0700

    chore: refactor to React

--- config/utils/index.js
+++
@@ -1,9 +0,0 @@
-import slugify from 'slugify'
-
-export const slugifyString = (str) => {
-  return slugify(str, {
-    replacement: '-',
-    remove: /[#,&,+()$~%.'":*?<>{}]/g,
-    lower: true,
-  })
-}
